4134c08ba4703c3333f07c49f8075f6267131101
danil0711 <danilmitsenko21@gmail.com>
2026-02-23 00:15:09 +0500

    minor changes

--- infrastructure/persistance/sqlite_snapshot_repository.py
+++ infrastructure/persistance/sqlite_snapshot_repository.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import sqlite3
 
 from application.interfaces.snapshot_repository import SnapshotRepository
-from domain.entitles import monitor
 from domain.entitles.snapshot import Snapshot
 
 
@@ -12,7 +11,7 @@ class SqliteSnapshotRepository(SnapshotRepository):
     """
 
     def __init__(self, db_path: str):
-        self.conn = sqlite3.connect(db_path)
+        self.conn = sqlite3.connect(db_path, check_same_thread=False)
         self._create_table()
 
     def _create_table(self):
@@ -32,7 +31,7 @@ class SqliteSnapshotRepository(SnapshotRepository):
             (snapshot.monitor_id, str(snapshot.value), snapshot.created_at.isoformat()),
         )
         self.conn.commit()
-        
+
     def get_last(self, monitor_id: str) -> Snapshot | None:
         cursor = self.conn.execute(
             """
@@ -42,17 +41,14 @@ class SqliteSnapshotRepository(SnapshotRepository):
             ORDER BY created_at DESC
             LIMIT 1
             """,
-            (monitor_id, )
+            (monitor_id,),
         )
-        
+
         row = cursor.fetchone()
-        
+
         if row is None:
             return None
-        
+
         return Snapshot(
-            monitor_id=row[0],
-            value=row[1],
-            created_at=datetime.fromisoformat(row[2])
+            monitor_id=row[0], value=row[1], created_at=datetime.fromisoformat(row[2])
         )
-        
